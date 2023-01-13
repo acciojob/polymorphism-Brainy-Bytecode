d69c616ff9932d9cbf550586b5f80da74c6571ba
Sushmita Singh <sushmitasachan97@gmail.com>
2023-01-13 13:24:36 +0530

    First Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,37 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static void main(String[] args) {
+        Product p=new Product();
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+
+        p.product(x, y);
+
+        int z=sc.nextInt();
+        p.product(x, y, z);
+
+        double a=sc.nextDouble();
+        double b=sc.nextDouble();
+        p.product(a, b);
+    }
+}
+
+class Product {
+
+    public int product(int x, int y) {
+       return 0;
+    }
+
+    public int product(int x, int y, int z) {
+       return 0;
+    }
+
+    public double product(double x, double y) {
+       return 0;
+    }
 }
